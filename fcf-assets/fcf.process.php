88d5cbb88bed3cd5e9fe4cbd7a32c5845249e101
chiragkpipal <114102441+chiragkpipal@users.noreply.github.com>
2023-02-02 21:57:25 +0530

    Add files via upload

---
+++ fcf-assets/fcf.process.php
@@ -0,0 +1,20 @@
+<html>
+  
+<meta http-equiv="refresh" content="0; URL=../success.php" />  
+<body bgcolor="black"></body>
+</html>
+<?php
+// ************************************
+// This file is part of a package from:
+// www.majesticform.com
+
+// Free Version
+// 2 January 2022
+
+// You are free to use an edit for 
+// your own use. But cannot resell
+// or repackage in any way.
+// ************************************
+
+require dirname(__FILE__).'/'.'fcf.config.php';
+require dirname(__FILE__).'/'.'process.php';
